e163d6ad0028c060cf4d1087e61e93d55c045869
Florian Sital Dahone <florian-sital-dahone@outlook.fr>
2018-08-20 14:48:44 +0200

    Ajout classes

---
+++ dtaction-android/Model/Liste.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace dtaction_android.Model
+{
+    class Liste
+    {
+        int Id { get; set; }
+        string Titre { get; set; }
+        List<Tache> Taches { get; set; }
+
+        public Liste()
+        {
+            Taches = new List<Tache>();
+        }
+    }
+}
---
+++ dtaction-android/Model/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace dtaction_android.Model
+{
+    class User
+    {
+        int Id { get; set; }
+        string Pseudo { get; set; }
+        string Email { get; set; }
+        string Mdp { get; set; }
+        string Url { get; set; }
+        List<Liste> Listes { get; set; }
+
+        public User()
+        {
+            Listes = new List<Liste>();
+        }
+    }
+}
