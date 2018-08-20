5a60de3de3a3f49893fb7b1ec5b752a19c6e91e0
Florian Sital Dahone <florian-sital-dahone@outlook.fr>
2018-08-20 16:22:21 +0200

    Ajout accueil

--- dtaction-android/MainActivity.cs
+++ dtaction-android/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Content;
 
 namespace dtaction_android
 {
@@ -14,6 +15,17 @@ namespace dtaction_android
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Button connexion = FindViewById<Button>(Resource.Id.main_connexion);
+            Button inscription = FindViewById<Button>(Resource.Id.main_inscription);
+
+            connexion.Click += delegate {
+                StartActivity(new Intent(this, typeof(ConnexionActivity)));
+            };
+
+            inscription.Click += delegate {
+                StartActivity(new Intent(this, typeof(InscriptionActivity)));
+            };
         }
     }
 }
